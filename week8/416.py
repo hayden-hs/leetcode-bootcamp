57007b36e32370954d2bff62c8eae202d3f23c41
hayden-hs <hr2712@nyu.edu>
2025-12-17 14:47:47 -0500

    Add files via upload

---
+++ week8/416.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+        
+        if total_sum % 2 != 0:
+            return False
+        
+        target = total_sum // 2
+        
+        dp = [False] * (target + 1)
+        dp[0] = True
+        
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                if dp[i - num]:
+                    dp[i] = True
+            
+            if dp[target]:
+                return True
+                
+        return dp[target] 
